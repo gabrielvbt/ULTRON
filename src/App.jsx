f4131008a9e07835761e0148e248ddf8bba64186
gabrielvbt <gabriel.vbt@hotmail.com>
2025-06-21 15:41:23 -0300

    first-commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <>
+      {/* Conteúdo do corpo do chat */}
+      <div id="particles-js"></div>
+      <canvas id="matrixCanvas" className="matrix-bg"></canvas>
+      <div className="chat-container futuristic-border">
+        {/* ... o restante da estrutura JSX vai aqui exatamente como estava */}
+      </div>
+    </>
+  );
+}
+
+export default App;
